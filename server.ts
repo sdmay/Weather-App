638226d92aff0d40436c53f43d35a0b4df061f38
sdmay <sdmay10@gmail.com>
2018-12-04 14:16:13 -0500

    Test

--- server.ts
+++ server.ts
@@ -2,11 +2,7 @@ import express from 'express';
 import * as forecast from './controllers/weather_check';
 const app = express();
 const PORT = process.env.PORT || 7777;
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res){
     
